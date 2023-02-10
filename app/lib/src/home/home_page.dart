86cd451278637c0685c709887c8e4b051e3613af
Paulo Filho <pcf.feat@gmail.com>
2023-02-10 14:43:35 -0400

    refactor: convert drawer into custom widget

--- app/lib/src/home/home_page.dart
+++ app/lib/src/home/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:listinha/src/home/widgets/custom_drawer.dart';
 import 'package:listinha/src/shared/widgets/user_image_button.dart';
 
 class HomePage extends StatefulWidget {
@@ -12,36 +13,7 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: NavigationDrawer(
-        children: [
-          Padding(
-            padding: const EdgeInsets.fromLTRB(16, 28, 16, 16),
-            child: Text(
-              'Opções',
-              style: Theme.of(context).textTheme.titleSmall,
-            ),
-          ),
-          NavigationDrawerDestination(
-            icon: const Icon(Icons.sync),
-            label: Row(
-              children: [
-                const Text('Sincronizar'),
-                const SizedBox(
-                  width: 25,
-                ),
-                Text(
-                  '12/12/2012 às 12:12',
-                  style: Theme.of(context).textTheme.bodySmall,
-                ),
-              ],
-            ),
-          ),
-          const NavigationDrawerDestination(
-            icon: Icon(Icons.settings),
-            label: Text('Configurações'),
-          ),
-        ],
-      ),
+      drawer: const CustomDrawer(),
       appBar: AppBar(
         title: const Text('LISTINHA'),
         actions: const [
