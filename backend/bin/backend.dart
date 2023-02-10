60d206be0e77256806228336f56f2ecacce2da15
Paulo Filho <pcf.feat@gmail.com>
2023-02-10 09:25:19 -0400

    app: create debugging environments for backend and app

--- backend/bin/backend.dart
+++ backend/bin/backend.dart
@@ -1,5 +1,10 @@
+import 'dart:io';
+
+import 'dart:io';
 import 'package:backend/backend.dart' as backend;
 
 void main(List<String> arguments) {
   print('Hello world: ${backend.calculate()}!');
+
+  stdin.readLineSync();
 }
