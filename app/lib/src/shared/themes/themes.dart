7183a6203587dc7da645885f5de7cf6640aedc39
Paulo Filho <pcf.feat@gmail.com>
2023-02-10 10:04:58 -0400

    refactor: improve hot reloading by converting final to a getter function

--- app/lib/src/shared/themes/themes.dart
+++ app/lib/src/shared/themes/themes.dart
@@ -1,24 +1,24 @@
 import 'package:flutter/material.dart';
 part 'color_schemes.g.dart';
 
-final lightTheme = ThemeData(
-  useMaterial3: true,
-  colorScheme: _lightColorScheme,
-  appBarTheme: AppBarTheme(
-    centerTitle: true,
-    backgroundColor: _lightColorScheme.primaryContainer,
-  ),
-  floatingActionButtonTheme: FloatingActionButtonThemeData(
-    backgroundColor: _lightColorScheme.primary,
-    foregroundColor: Colors.white,
-  ),
-);
+ThemeData get lightTheme => ThemeData(
+      useMaterial3: true,
+      colorScheme: _lightColorScheme,
+      appBarTheme: AppBarTheme(
+        centerTitle: true,
+        backgroundColor: _lightColorScheme.primaryContainer,
+      ),
+      floatingActionButtonTheme: FloatingActionButtonThemeData(
+        backgroundColor: _lightColorScheme.primary,
+        foregroundColor: Colors.white,
+      ),
+    );
 
-final darkTheme = ThemeData(
-  useMaterial3: true,
-  colorScheme: _darkColorScheme,
-  appBarTheme: AppBarTheme(
-    centerTitle: true,
-    backgroundColor: _darkColorScheme.primaryContainer,
-  ),
-);
+ThemeData get darkTheme => ThemeData(
+      useMaterial3: true,
+      colorScheme: _darkColorScheme,
+      appBarTheme: AppBarTheme(
+        centerTitle: true,
+        backgroundColor: _darkColorScheme.primaryContainer,
+      ),
+    );
