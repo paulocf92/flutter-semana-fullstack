4825f8de84269a8715972205ffa1ef8681d6b89e
Paulo Filho <pcf.feat@gmail.com>
2023-02-10 10:12:42 -0400

    refactor: reorganize the layout of widgets and home page

---
+++ app/lib/src/app_widget.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:listinha/main.dart';
+import 'package:listinha/src/home/home_page.dart';
+
+import 'shared/themes/themes.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.light,
+      theme: lightTheme,
+      darkTheme: darkTheme,
+      home: HomePage(),
+    );
+  }
+}
