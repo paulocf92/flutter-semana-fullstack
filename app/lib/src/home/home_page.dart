99f054a3028a90b304560245d5209111a085fa18
Paulo Filho <pcf.feat@gmail.com>
2023-02-10 11:04:10 -0400

    chore: add segmented button list to home page

--- app/lib/src/home/home_page.dart
+++ app/lib/src/home/home_page.dart
@@ -21,6 +21,35 @@ class _HomePageState extends State<HomePage> {
           ),
         ],
       ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: SegmentedButton<int>(
+              segments: const [
+                ButtonSegment(
+                  value: 0,
+                  label: Text('Todos'),
+                ),
+                ButtonSegment(
+                  value: 1,
+                  label: Text('Pendentes'),
+                ),
+                ButtonSegment(
+                  value: 2,
+                  label: Text('Concluídas'),
+                ),
+                ButtonSegment(
+                  value: 3,
+                  label: Text('Desativadas'),
+                ),
+              ],
+              selected: const {0},
+              onSelectionChanged: (values) {},
+            ),
+          ),
+        ],
+      ),
       floatingActionButton: FloatingActionButton.extended(
         icon: const Icon(Icons.edit),
         label: const Text('Nova Lista'),
