ce6ece7b71d4719b9bfa02541a6a3d4c554229ab
Paulo Filho <pcf.feat@gmail.com>
2023-02-12 16:35:24 -0400

    chore: flesh out settings page

--- app/lib/src/configuration/configuration_page.dart
+++ app/lib/src/configuration/configuration_page.dart
@@ -14,6 +14,62 @@ class _ConfigurationPageState extends State<ConfigurationPage> {
       appBar: AppBar(
         title: const Text('LISTINHA'),
       ),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Configurações',
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Text(
+              'Tema',
+              style: Theme.of(context).textTheme.titleMedium,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            RadioListTile<ThemeMode>(
+              value: ThemeMode.system,
+              groupValue: ThemeMode.light,
+              title: const Text('Sistema'),
+              onChanged: (mode) {},
+            ),
+            RadioListTile<ThemeMode>(
+              value: ThemeMode.light,
+              groupValue: ThemeMode.light,
+              title: const Text('Claro'),
+              onChanged: (mode) {},
+            ),
+            RadioListTile<ThemeMode>(
+              value: ThemeMode.dark,
+              groupValue: ThemeMode.light,
+              title: const Text('Escuro'),
+              onChanged: (mode) {},
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Text(
+              'Controle de dados',
+              style: Theme.of(context).textTheme.titleMedium,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            OutlinedButton(
+              onPressed: () {},
+              child: const Text(
+                'Apagar cache e reiniciar o app',
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
