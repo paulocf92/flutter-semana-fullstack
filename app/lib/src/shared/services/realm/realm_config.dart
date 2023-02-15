54b5370556d57144f82ae1c0f0b42f5441a01a30
Paulo Filho <pcf.feat@gmail.com>
2023-02-15 14:20:50 -0400

    feat: tune in app with a realm-powered data persistence through dependency injection

---
+++ app/lib/src/shared/services/realm/realm_config.dart
@@ -0,0 +1,10 @@
+import 'package:listinha/src/shared/services/realm/models/configuration_model.dart';
+import 'package:realm/realm.dart';
+
+var config = Configuration.local(
+  [ConfigurationModel.schema],
+  initialDataCallback: (realm) {
+    realm.add(ConfigurationModel('system'));
+  },
+  path: './realm/realm.db',
+);
--- app/lib/src/shared/stores/app_store.dart
+++ app/lib/src/shared/stores/app_store.dart
@@ -1,7 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:listinha/src/configuration/services/configuration_service.dart';
 
 class AppStore {
   final themeMode = ValueNotifier(ThemeMode.system);
+  final syncDate = ValueNotifier<DateTime?>(null);
+
+  final ConfigurationService _configurationService;
+
+  AppStore(this._configurationService) {
+    init();
+  }
+
+  void init() {
+    final model = _configurationService.getConfiguration();
+
+    syncDate.value = model.syncDate;
+    themeMode.value = _getThemeModeByName(model.themeModeName);
+  }
+
+  void save() {
+    _configurationService.saveConfiguration(
+      themeMode.value.name,
+      syncDate.value,
+    );
+  }
+
   void changeThemeMode(ThemeMode? mode) {
     if (mode != null) {
       themeMode.value = mode;
@@ -9,16 +32,16 @@ class AppStore {
     }
   }
 
-  final syncDate = ValueNotifier<DateTime?>(null);
-
   void setSyncDate(DateTime date) {
     syncDate.value = date;
     save();
   }
 
-  void save() {
-    // TODO: Salvar os dados na base local
+  void deleteApp() {
+    _configurationService.deleteAll();
   }
 
-  void init() {}
+  ThemeMode _getThemeModeByName(String name) {
+    return ThemeMode.values.firstWhere((mode) => mode.name == name);
+  }
 }
