51aa5c0da39803cfc43c0e67177d17206da3c2fe
Paulo Filho <pcf.feat@gmail.com>
2023-02-23 15:39:40 -0400

    refactor: improve separation of concerns by pulling data management off AppStore, add reactivity through state with RxNotifier

--- app/lib/src/configuration/configuration_page.dart
+++ app/lib/src/configuration/configuration_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:listinha/src/shared/stores/app_store.dart';
+import 'package:rx_notifier/rx_notifier.dart';
 
 class ConfigurationPage extends StatefulWidget {
   const ConfigurationPage({super.key});
@@ -10,11 +11,17 @@ class ConfigurationPage extends StatefulWidget {
 }
 
 class _ConfigurationPageState extends State<ConfigurationPage> {
+  final appStore = Modular.get<AppStore>();
+
+  void _changeThemeMode(ThemeMode? mode) {
+    if (mode != null) {
+      appStore.themeMode.value = mode;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    final appStore = context.watch<AppStore>(
-      (store) => store.themeMode,
-    );
+    context.select(() => appStore.themeMode.value);
 
     return Scaffold(
       appBar: AppBar(
@@ -43,19 +50,19 @@ class _ConfigurationPageState extends State<ConfigurationPage> {
               value: ThemeMode.system,
               groupValue: appStore.themeMode.value,
               title: const Text('Sistema'),
-              onChanged: appStore.changeThemeMode,
+              onChanged: _changeThemeMode,
             ),
             RadioListTile<ThemeMode>(
               value: ThemeMode.light,
               groupValue: appStore.themeMode.value,
               title: const Text('Claro'),
-              onChanged: appStore.changeThemeMode,
+              onChanged: _changeThemeMode,
             ),
             RadioListTile<ThemeMode>(
               value: ThemeMode.dark,
               groupValue: appStore.themeMode.value,
               title: const Text('Escuro'),
-              onChanged: appStore.changeThemeMode,
+              onChanged: _changeThemeMode,
             ),
             const SizedBox(
               height: 20,
